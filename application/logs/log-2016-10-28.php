abc0ea4ff54f4b7ea2454e4bec239502b8287de2
David Valdez <david@hotmail.com>
2016-10-28 09:55:52 -0500

    set evironments

---
+++ application/logs/log-2016-10-28.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-10-28 09:45:55 --> Severity: Error --> Class 'CI_Lan' not found /var/www/blog.com/public_html/application/core/MY_Lang.php 5
